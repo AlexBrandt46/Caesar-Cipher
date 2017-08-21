09c80f278f91a42af64946dd800363b2490a8cee
AlexBrandt46 <ajb3155@gmail.com>
2017-08-20 20:15:33 -0400

    Added most of method "Decrypt"

--- Caesar Cipher/Program.cs
+++ Caesar Cipher/Program.cs
@@ -13,7 +13,8 @@ namespace Caesar_Cipher {
         static void Main(string[] args) {
 
             string message; //the message entered by the user
-            
+            string response = ""; //the response to whether or not the user would like the message decrypted
+
             //the user entered message
             Console.WriteLine("Please enter the message that you'd like to encrypt:");
             message = Console.ReadLine();
@@ -26,14 +27,31 @@ namespace Caesar_Cipher {
             
             Console.WriteLine();
 
-            string encrypted = ""; //encrypted version of the message
-            string decrypted = ""; //decrypted version of the message
+            StringBuilder encrypted = new StringBuilder(); //encrypted version of the message
+            StringBuilder decrypted = new StringBuilder(); //decrypted version of the message
 
-            encrypted = Encrypt(shift, message).ToString();
+            while (true) {
 
-            Console.WriteLine("The encrypted version of your message is: ");
-            Console.WriteLine(encrypted);
+                encrypted = Encrypt(shift, message);
+                
+                Console.WriteLine("The encrypted version of your message is: ");
+                Console.WriteLine(encrypted);
+                Console.WriteLine();
+                Console.Write("Would you like to decrypt your message? Please type yes or no: ");
+                response = Console.ReadLine().ToLower();
 
+                if (response.Equals("no")) {
+                    break;
+                }
+                else {
+                    decrypted = Decrypt(shift, encrypted.ToString());
+                }
+
+                Console.WriteLine("The decrypted message is: ");
+                Console.Write(decrypted);
+
+                break;
+            }
         }
         
         //encrypts the message; shift is the amount of letters that the characters shift by, and message is the message that the user inputs
@@ -57,12 +75,38 @@ namespace Caesar_Cipher {
 
                     }
                 }
-            } 
+            }
 
             return encryption;
         }
+
         public static StringBuilder Decrypt(int shift, string message) {
 
+            StringBuilder decryption = new StringBuilder();
+            char[] messageArray = message.ToCharArray();
+            char shiftedLetter = ' ';
+
+            for (int i = 0; i < messageArray.Length; i++) {
+
+                for (int a = 0; a < alphabet.Length; a++) {
+
+                    if (messageArray[i].Equals(alphabet[a])) {
+
+                        //takes the letter in the alphabet array which is equal to the current letter in the message array
+                        //it subtracts the shift value to the index value of the alphabet array's current index value and then performs 
+                        //mod 26 on it to ensure that it's always in the alphabet and there's no array out of bounds error
+                        shiftedLetter = alphabet[((a - shift) % 26)];
+                        decryption.Append(shiftedLetter);
+                        Console.WriteLine(decryption);
+                    }
+                }
+            }
+
+            return decryption;
         }
     }
 }
+
+/*TODO:
+ *  #separate spaces from the alphabet array
+ */
