ac4186b472039ff45f93fa918180258634152a05
AlexBrandt46 <ajb3155@gmail.com>
2017-09-02 17:31:35 -0400

    Started the GUI Component of the Project

---
+++ Caesar Cipher/GUI.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Caesar_Cipher {
+    public partial class GUI : Form {
+        public GUI() {
+            InitializeComponent();
+
+        }
+    }
+}
